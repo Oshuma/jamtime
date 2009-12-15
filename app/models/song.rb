154561662aaedf416f9ea036e2f59c60462a6fee
Dale Campbell <oshuma@gmail.com>
2009-12-15 03:12:53 -0600

    song specs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,37 @@
 class Song < ActiveRecord::Base
   validates_presence_of :path
-end
+
+  before_save :update_tag
+
+  private
+
+  def update_tag
+    begin
+      Mp3Info.open(path) do |mp3|
+        tag = mp3.tag
+        [ :title, :artist, :album, :genre ].each do |id3_tag|
+          id3_value = tag.send(id3_tag)
+          if id3_value
+            self.send("#{id3_tag}=", id3_value)
+          end
+        end
+      end
+    rescue => error
+      Rails.logger.error "Not a music file: #{error}"
+    end
+  end
+
+  class << self
+    def find_by_path(path)
+      return nil unless File.exists?(path)
+      find_or_create_by_path(path)
+    end
+
+    def find_by_path!(*args)
+      song = find_by_path(*args)
+      raise ActiveRecord::RecordNotFound unless song
+      song
+    end
+  end # self
+
+end # Song
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Song do
   before(:all) do
     remove_test_songs
+    @song_path = File.join(::Rails.root, 'spec', 'music', 'song1.mp3')
   end
 
   before(:each) do
@@ -14,4 +15,32 @@ describe Song do
       Factory(:song, :path => nil)
     end.should raise_error(ActiveRecord::RecordInvalid)
   end
+
+  it 'should find the song by path' do
+    @song = Song.find_by_path(@song_path)
+    @song.should_not be_nil
+    @song.path.should == @song_path
+  end
+
+  it "should return nil if the music file isn't found" do
+    @song = Song.find_by_path('/not/a/real/path')
+    @song.should be_nil
+  end
+
+  it 'should raise a not found exception' do
+    lambda do
+      Song.find_by_path!('/not/a/real/path')
+    end.should raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it 'should not recreate the database record' do
+  end
+
+  describe 'ID3 tag' do
+    it 'has a title' do
+      @song = Song.find_by_path(@song_path)
+      @song.title.should_not be_nil
+    end
+  end
+
 end
