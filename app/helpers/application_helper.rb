50de406e9b808e4fd3581595d2b22bf71fa74d5c
Dale Campbell <oshuma@gmail.com>
2009-12-17 05:44:55 -0600

    temp layout shit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,32 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def link_with_active(name, path)
+    opts = { :class => 'active' } if current_page?(path)
+    content_tag :li, opts do
+      link_to(name, path)
+    end
+  end
+
+  def messages_waiting?
+    flash[:notice] || flash[:error]
+  end
+
+  def error_message
+    return unless flash[:error]
+    message_content(:error)
+  end
+
+  def notice_message
+    return unless flash[:notice]
+    message_content(:notice)
+  end
+
+  private
+
+  def message_content(type)
+    opts = { :id => "#{type.to_s}-message", :class => 'message' }
+    content_tag(:div, opts) { flash[type] }
+  end
+
 end
