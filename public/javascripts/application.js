56a316f6a5a5ee0fbd3db1cae288a021ea41965b
Dale Campbell <oshuma@gmail.com>
2010-01-04 08:40:09 -0600

    minor

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $('#collapse-subfolders').live('click', function() {
-    window.location.hash = null;
+    window.location.hash = '';
     $('.subfolder').hide();
     return false;
   });
