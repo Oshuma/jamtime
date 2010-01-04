d895c042c7f5e64182286e5cc84c27ee8d023c06
Dale Campbell <oshuma@gmail.com>
2010-01-04 08:46:46 -0600

    initial theme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  layout proc { |controller| controller.request.xhr? ? nil : 'application' }
+  layout proc { |controller| controller.request.xhr? ? nil : 'jamtime' }
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
