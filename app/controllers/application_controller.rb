fcb8eced0fd41a76eb32815893cc708e141f4270
Dale Campbell <oshuma@gmail.com>
2010-01-05 15:34:24 -0600

    starting on auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  # include Authentication
-  # helper_method :current_user, :current_user_session, :logged_in?, :admin?
+  include Authentication
 
   helper :all # include all helpers, all the time
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -13,3 +13,10 @@ Factory.define(:song) do |s|
   s.sequence(:title) { |i| "Song #{i}" }
   s.sequence(:path)   { |i| create_song("test#{i}.mp3") }
 end
+
+Factory.define(:user) do |u|
+  u.sequence(:login) { |i| "luser#{i}" }
+  u.email { |user| "#{user.login}@example.com" }
+  u.password 'sekretz'
+  u.password_confirmation { |user| user.password }
+end
